01e8fc0b9ed22378e10e04ddbe3ef62a42956fe5
“fredrickUdom” <fredrickudom@gmail.com>
2024-01-12 11:44:31 +0100

    adding enable cors in the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 // import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const port = process.env.PROJECT_PORT;
+  const port = process.env.PROJECT_PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+
+  app.enableCors({
+    origin: 'http://localhost:3000'
+  })
   // app.use(cookieParser());
   app.setGlobalPrefix('api/v1')
   await app.listen(port, () => console.log(`server is running on port: ${port}`));
